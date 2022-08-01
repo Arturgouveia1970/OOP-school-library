739fcf585c2706538087ca00b09280127e3c3839
Arturgouveia1970 <artgouveiag@gmail.com>
2022-08-01 16:42:51 +0100

    changes suggested by reviewer

--- student.rb
+++ student.rb
@@ -2,9 +2,8 @@ require_relative './person'
 
 class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, parent_permission: parent_permission)
-    @classroom = classroom
-    @name = name
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom  
   end
 
   def play_hooky
--- teacher.rb
+++ teacher.rb
@@ -2,12 +2,12 @@ require_relative './person'
 
 class Teacher < Person
   def initialize(specialization, age, name = 'Unknown', parent_permission: true)
-    super(age, parent_permission: parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
-    @name = name
   end
 
   def can_use_services?
     true
   end
 end
+
