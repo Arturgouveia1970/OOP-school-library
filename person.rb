8d0cca7f5f6fdac4b2d811b582d6d44141728501
Arturgouveia1970 <artgouveiag@gmail.com>
2022-08-02 16:23:47 +0100

    more changes

--- person.rb
+++ person.rb
@@ -27,10 +27,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts (person.correct_name)
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts (capitalizedPerson.correct_name)
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts (capitalizedTrimmedPerson.correct_name)
