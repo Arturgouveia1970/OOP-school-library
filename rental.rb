352f07d499998cf06321616ba725a9b1c5735596
Arturgouveia1970 <artgouveiag@gmail.com>
2022-08-03 17:05:16 +0100

    linters

--- rental.rb
+++ rental.rb
@@ -1,14 +1,12 @@
 require_relative './book'
 require_relative './person'
 
-
 class Rental
   attr_reader :book, :person
   attr_accessor :date
 
   def initialize(person, book, date)
     @date = date
-    
     @person = person
     person.rentals << self
 
