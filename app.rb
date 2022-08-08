4574d6f73d94ed97b2c1db514c3e8bffada0e8f6
arturgouveia1970 <artgouveiag@gmail.com>
2022-08-08 11:23:30 +0100

    moved user choice to app.rb

--- app.rb
+++ app.rb
@@ -136,4 +136,26 @@ class Apps
   def convert_date(str)
     Date.parse(str)
   end
+
+  def user_choice(choice)
+    case choice
+    when 1
+      list_books
+    when 2
+      list_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rentals
+    when 7
+      puts 'Thank you for using the school library app'
+      exit
+    else
+      puts ' Choose a number between 1 and 7'
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -1,54 +1,31 @@
 require './app'
 
-class Apps
-  def self.main_menu
-    puts 'Welcome to the School Library App!'
-    puts "\n"
-    puts 'Please choose an option by entering a number: '
+def apps
+  puts 'Welcome to the School Library App!'
+  puts "\n"
+  puts 'Please choose an option by entering a number: '
 
-    @list = {
-      '1' => 'list all books',
-      '2' => 'list all people',
-      '3' => 'Create a person',
-      '4' => 'Create a book',
-      '5' => 'Create a rental',
-      '6' => 'List all rentals for a given person id',
-      '7' => 'Exit'
-    }
+  list = {
+    '1' => 'list all books',
+    '2' => 'list all people',
+    '3' => 'Create a person',
+    '4' => 'Create a book',
+    '5' => 'Create a rental',
+    '6' => 'List all rentals for a given person id',
+    '7' => 'Exit'
+  }
 
-    @list.each do |index, string|
-      puts "#{index} - #{string}"
-    end
-
-    Integer(gets.chomp)
+  list.each do |index, string|
+    puts "#{index} - #{string}"
   end
 
-  run = Apps.new
-  loop do
-    case main_menu
-    when 1
-      run.list_books
-    when 2
-      run.list_people
-    when 3
-      run.create_person
-    when 4
-      run.create_book
-    when 5
-      run.create_rental
-    when 6
-      run.list_rentals
-    when 7
-      puts 'Thank you for using the school library app'
-      exit
-    else
-      puts ' Choose a number between 1 and 7'
-    end
-  end
+  choice = gets.chomp
+  Apps.user_choice(choice)
 end
 
 def main
-  App.new
+  Apps.new
+  apps
 end
 
 main
