d69b6e769ecd97e7749ed6713e96f859d67f8cbc
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-08 15:29:04 +0300

    Move user choice method to new file

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require_relative './teacher'
 require_relative './student'
 require_relative './rental'
 require 'date'
+require './menu'
 
 # app class
 class Apps
@@ -135,27 +136,5 @@ class Apps
   # convert date
   def convert_date(str)
     Date.parse(str)
-  end
-
-  def user_choice(choice)
-    case choice
-    when 1
-      list_books
-    when 2
-      list_people
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      list_rentals
-    when 7
-      puts 'Thank you for using the school library app'
-      exit
-    else
-      puts ' Choose a number between 1 and 7'
-    end
   end
 end
--- main.rb
+++ main.rb
@@ -1,31 +1,9 @@
 require './app'
-
-def apps
-  puts 'Welcome to the School Library App!'
-  puts "\n"
-  puts 'Please choose an option by entering a number: '
-
-  list = {
-    '1' => 'list all books',
-    '2' => 'list all people',
-    '3' => 'Create a person',
-    '4' => 'Create a book',
-    '5' => 'Create a rental',
-    '6' => 'List all rentals for a given person id',
-    '7' => 'Exit'
-  }
-
-  list.each do |index, string|
-    puts "#{index} - #{string}"
-  end
-
-  choice = gets.chomp
-  Apps.user_choice(choice)
-end
+require './user_choice'
 
 def main
-  Apps.new
-  apps
+  choice = menu
+  user_choice(choice)
 end
 
 main
